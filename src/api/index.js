b88f3496275417b5e3191233f43673747dacf040
30percentoff <s9617129172@gmail.com>
2025-12-13 17:58:38 +0800

    註解＿CROS允許外部請求

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,19 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'http://127.0.0.1:8000/api',//http://192.168.0.45:3001/api //本地伺服器IP
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   }
 })
+
+//CROS允許外部請求
+/*const cors = require('cors'); 
+app.use(cors({
+  origin: '*', // 測試時建議先全開，允許任何來源
+  // 或者指定特定 IP: origin: 'http://ip/'
+}));*/
 
 // 獲取國家狀態
 export const getCountryStatus = () => {
